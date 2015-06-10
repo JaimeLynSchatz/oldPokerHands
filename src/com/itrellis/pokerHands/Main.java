878ceb67bdffffa7566035f3efb63af4ed903da7
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2015-06-09 23:26:06 -0700

    add rules for hands - now to create hand objects

--- src/com/itrellis/pokerHands/Main.java
+++ src/com/itrellis/pokerHands/Main.java
@@ -2,7 +2,6 @@ package com.itrellis.pokerHands;
 
 // Da Rules
 //
-//
 //        Sample Input
 //        2H 3D 5S 9C KD 2C 3H 4S 8C AH
 //        2H 4S 4C 2D 4H 2S 8S AS QS 3S
@@ -20,5 +19,23 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        // Straight Flush: same suit, 5 consecutive cards, ranked by highest card
+
+        // Four of a Kind: four cards, same rank, ranked by card rank
+
+        // Full house: three of a kind, and a pair, ranked by the value of the three cards
+
+        // Flush: five cards, same suit, any values, ranked using High Card rules
+
+        // Straight: Hand contains five cars with consecutive values, ranked by highest card
+
+        // Three of a kind, ranked by value of the three
+
+        // Two pairs, ranked by highest, 2nd ranking 2nd paid, 3rd ranking single card
+
+        // Pair, two cards with same value, ranked by value of pair, followed by remaining cards, starting w/ lowest
+
+        // High Card: Single highest card, beat ties with values of remaining cards (starting with highest)
     }
 }
