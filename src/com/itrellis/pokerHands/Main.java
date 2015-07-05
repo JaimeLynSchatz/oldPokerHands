b03511f40e12b638b166e9ddfc0e7b5977c61ed0
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2015-07-05 08:01:58 -0700

    deleting most of the code, starting again

--- src/com/itrellis/pokerHands/Main.java
+++ src/com/itrellis/pokerHands/Main.java
@@ -27,55 +27,41 @@ public class Main {
             private String fullCard = "";
             private String rank = "";
             private String suit = "";
-
-            public String Card(String rankAndSuit) {
-                this.rank = toString(rankAndSuit.charAt(0));
-                this.rank = toString(rankAndSuit.charAt(1));
-                this.fullCard = "something good here";
-                return this.fullCard;
-            }
         }
+
         class Hand {
             // Does this need to be a List or would an array work? Or is it better to use Collections on principle?
-            List<String> cards = new ArrayList<String>();
-
-            Map<Character, Integer> suits = new HashMap<Character, Integer>();
-            Map<Character, Integer> ranks = new HashMap<Character, Integer>();
-
-            public Hand(String[] playerHand){
-                // tried starting at 0 for collection, but can't do that for ranks -- too much
-                for (String card : playerHand) {
-                    cards.add(card);
-                    int currentRankCount = ranks.get(card.charAt(0));
-                    int currentSuitCount = ranks.get(card.charAt(1));
-                    ranks.put(card.charAt(0), ++currentRankCount);  // can't test with null comparison
-                    suits.put(card.charAt(1), ++currentSuitCount);  // will primatives default to zero??
-                }
-            }
+            List<Card> cards = new ArrayList<>();
         }
 
-        // Take the hand of cards and go through it in a cycle, making lists<> of the suits, ranks, etc.
-        // Then have rules regarding those lists.
-        String[] firstRound = {"2H", "3D", "5D", "9C", "KD", "2C", "3H", "4S", "8C", "AH"};
-        Hand player = new Hand(Arrays.copyOfRange(firstRound, 0, firstRound.length/2));
-        System.out.println(player);
+        // Take the list, make Cards and then two Hands out of the cards
+        String firstRound = "2H 3D 5S 9C KD 2C 3H 4S 8C AH";
+        String[] cardArray = new String[10];
+        cardArray = firstRound.split(" ");
+        for (String card : cardArray) {
+            System.out.println(card);
+        }
 
-        Hand house = new Hand(Arrays.copyOfRange(firstRound, firstRound.length/2, firstRound.length - 1));
+        // this is for the old method of creating hands
+        //Hand player = new Hand(Arrays.copyOfRange(firstRound, 0, firstRound.length/2));
+        //System.out.println(player);
 
+        //Hand house = new Hand(Arrays.copyOfRange(firstRound, firstRound.length/2, firstRound.length - 1));
+        //System.out.println(house);
 
 
         // Straight Flush: same suit, 5 consecutive cards, ranked by highest card
-        if (player.suits.size() == 1) { // then they're all the same suit
+        //if (player.suits.size() == 1) { // then they're all the same suit
             // test here for consecutive ranks
-        }
+        //}
 
         // Four of a Kind: four cards, same rank, ranked by card rank
-        if (player.ranks.size() == 2) {  // this won't work --
-            System.out.println("Four of a kind!!");
+        //if (player.ranks.size() == 2) {  // this won't work --
+          //  System.out.println("Four of a kind!!");
             // no real way to retrieve value
             // test for rank of fifth card
 
-        }
+        //}
 
         // Full house: three of a kind, and a pair, ranked by the value of the three cards
 
